369ec97c6f75482f4b8aa90465012073e8bdfbc3
Alan <wyffcld@163.com>
2020-10-09 15:51:22 +0800

    html插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 let path = require('path')
+let HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-    mode: 'development', // production development
+    devServer: {
+        port: 3000,
+        progress: true, // 进度条
+        contentBase: './build',
+        compress: true, // 压缩
+    },
+    mode: 'production', // production development
     entry: './src/index.js', // 入口
     output: {
-        filename: 'bundle.js', // 打包后的文件名
+        filename: 'bundle.[hash:8].js', // 打包后的文件名
         path: path.resolve(__dirname, 'build'), // 必须是绝对路径
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            minify: {
+                removeAttributeQuotes: true, // 删除双引号
+                collapseWhitespace: true, // 折叠空行
+
+            },
+            hash: true
+        })
+    ]
 }
