95b60119b8ae282c0c30a331017ccaf298e5ee93
Alan <wyffcld@163.com>
2020-10-10 13:35:29 +0800

    样式处理1

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         contentBase: './build',
         compress: true, // 压缩
     },
-    mode: 'production', // production development
+    mode: 'development', // production development
     entry: './src/index.js', // 入口
     output: {
         filename: 'bundle.[hash:8].js', // 打包后的文件名
@@ -20,9 +20,68 @@ module.exports = {
             minify: {
                 removeAttributeQuotes: true, // 删除双引号
                 collapseWhitespace: true, // 折叠空行
-
             },
             hash: true
         })
-    ]
+    ],
+    module: {
+        // 模块
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: 'style-loader',
+                    options: {
+                        insert: function insertAtTop(element) {
+                            var parent = document.querySelector('head');
+                            // eslint-disable-next-line no-underscore-dangle
+                            var lastInsertedElement =
+                                window._lastElementInsertedByStyleLoader;
+
+                            if (!lastInsertedElement) {
+                                parent.insertBefore(element, parent.firstChild);
+                            } else if (lastInsertedElement.nextSibling) {
+                                parent.insertBefore(element, lastInsertedElement.nextSibling);
+                            } else {
+                                parent.appendChild(element);
+                            }
+
+                            // eslint-disable-next-line no-underscore-dangle
+                            window._lastElementInsertedByStyleLoader = element;
+                        },
+                    }
+                }, 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            insert: function insertAtTop(element) {
+                                var parent = document.querySelector('head');
+                                // eslint-disable-next-line no-underscore-dangle
+                                var lastInsertedElement =
+                                    window._lastElementInsertedByStyleLoader;
+
+                                if (!lastInsertedElement) {
+                                    parent.insertBefore(element, parent.firstChild);
+                                } else if (lastInsertedElement.nextSibling) {
+                                    parent.insertBefore(element, lastInsertedElement.nextSibling);
+                                } else {
+                                    parent.appendChild(element);
+                                }
+
+                                // eslint-disable-next-line no-underscore-dangle
+                                window._lastElementInsertedByStyleLoader = element;
+                            },
+                        }
+                    },
+                    'css-loader',
+                    'less-loader'
+                ]
+            }
+        ]
+
+    }
 }
