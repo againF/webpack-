606f7e7dcf39e15e80119f8156ecb4cff69ca7f9
Alan <wyffcld@163.com>
2020-10-20 09:55:22 +0800

    使用watch

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,12 @@ module.exports = {
         filename: '[name].[hash:4].bundle.js',
         path: path.resolve(__dirname, 'build')
     },
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/, // 对于某些系统，监听大量文件系统会导致大量的 CPU 或内存占用。这个选项可以排除一些巨大的文件夹，例如 node_modules
+        poll: 1000, // 每秒检查一次变动
+        aggregateTimeout: 600 // 当第一个文件更改，会在重新构建前增加延迟。这个选项允许 webpack 将这段时间内进行的任何其他更改都聚合到一次重新构建里。以毫秒为单位
+    },
     plugins: [
         // new HtmlWebpackPlugin({
         //     template: './src/index.html',
